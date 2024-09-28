3bd1ce7634cfdfab0320f36e55b34813a715e402
Rorisang Putu <rorisangputu@gmail.com>
2024-09-28 18:24:21 +0200

    Creating App User

---
+++ GogApp/Models/AppUser.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace GogApp.Models;
+
+public class AppUser : IdentityUser
+{
+    // Projects the user manages (they are the creator/manager)
+    public ICollection<Project>? MyProjects { get; set; }
+
+    // Projects the user is volunteering for (not managing)
+    public ICollection<ProjectVolunteer>? ProjectVolunteers { get; set; }
+
+    // Tasks assigned to the user as a volunteer
+    public ICollection<TaskVolunteer>? TaskVolunteers { get; set; }
+}
