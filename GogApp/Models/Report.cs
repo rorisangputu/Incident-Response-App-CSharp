bc7192fb0e680b9e6d0ea16b7a403bf5c5cad486
Rorisang Putu <rorisangputu@gmail.com>
2024-09-28 18:28:25 +0200

    creating report model

---
+++ GogApp/Models/Report.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GogApp.Models;
+
+public class Report
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string? Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    [ForeignKey("Project")]
+    public int ProjectId { get; set; }
+    public Project? Project { get; set; }
+}
