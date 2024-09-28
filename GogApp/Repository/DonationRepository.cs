fffbb87886f162b8a9e17da8480145bcceeff0d9
Rorisang Putu <rorisangputu@gmail.com>
2024-09-28 22:27:02 +0200

    creating donation repo

---
+++ GogApp/Repository/DonationRepository.cs
@@ -0,0 +1,34 @@
+using System;
+using GogApp.Data;
+using GogApp.Interfaces;
+using GogApp.Models;
+
+namespace GogApp.Repository;
+
+public class DonationRepository : IDonationRepository
+{
+    private readonly ApplicationDbContext context;
+
+    public DonationRepository(ApplicationDbContext dbContext)
+    {
+        context = dbContext;
+    }
+    public async Task<bool> Add(Donation donation)
+    {
+        context.Add(donation);
+        return await SaveAsync();
+    }
+
+    public async Task<IEnumerable<Donation>> GetDonationsByProjectId(int projectId)
+    {
+        return await context.Donations
+            .Where(d => d.ProjectId == projectId)
+            .ToListAsync();
+    }
+
+    public async Task<bool> SaveAsync()
+    {
+        var saved = await context.SaveChangesAsync();
+        return saved > 0 ? true : false;
+    }
+}
