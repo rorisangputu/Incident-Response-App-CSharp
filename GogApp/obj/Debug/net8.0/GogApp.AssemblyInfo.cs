2d20513fa70ab0771d507d0b9a2204e876f96e03
Rorisang Putu <rorisangputu@gmail.com>
2024-09-28 22:27:12 +0200

    additions

--- GogApp/obj/Debug/net8.0/GogApp.AssemblyInfo.cs
+++ GogApp/obj/Debug/net8.0/GogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fae00eba207954582bc9494641180e0065c0336")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6ef4261191ec57a9fd3094719239d7c0142d27")]
 [assembly: System.Reflection.AssemblyProductAttribute("GogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
